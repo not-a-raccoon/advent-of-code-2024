8c03a832c32903bfe1cfee4c4cc5e5a61b7cc379
not-a-raccoon <11160484+not-a-raccoon@users.noreply.github.com>
2024-12-06 19:18:55 +0100

    day 6, task 2

--- 2024-12-06/2024-12-06.py
+++ 2024-12-06/2024-12-06.py
@@ -1,6 +1,7 @@
 from itertools import product
 import sys
 import logging
+import copy
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
@@ -20,7 +21,7 @@ def get_guard(array):
     x, y = [(x,y) for x in range(len(array)) for y in range(len(array[0])) if array[y][x]=="^"][0]
     return x, y
 
-def step(array, guard_x, guard_y, direction, num_steps, positions_visited):
+def step(array, visited, guard_x, guard_y, direction, num_steps, positions_visited):
     assert 0 <= direction < 4
     logging.debug(f"At position (x={guard_x}, y={guard_y}), in direction {directions[direction][3]}. We have visited {positions_visited} positions.")
 
@@ -44,11 +45,16 @@ def step(array, guard_x, guard_y, direction, num_steps, positions_visited):
     if array[new_y][new_x] == "#":
         # tile is blocked, rotate clockwise
         logging.debug("Path is blocked, rotating clockwise.")
-        return step(array, guard_x, guard_y, (direction+1) % 4, num_steps + 1, positions_visited)
+        if visited[guard_y][guard_x] & directions[(direction+1) % 4][2] > 0:
+            # tile has been visited before, in this direction
+            logging.debug(
+                f"This place looks familiar. Afters: {num_steps} steps, and with {positions_visited} positions visited.")
+            return 2, num_steps, positions_visited
+        return step(array, visited, guard_x, guard_y, (direction+1) % 4, num_steps + 1, positions_visited)
 
     # Visit new tile (or known tile, but in new direction):
     is_new_tile = visited[new_y][new_x] == 0
-    return step(array, new_x, new_y, direction, num_steps + 1, positions_visited + 1 if is_new_tile else positions_visited)
+    return step(array, visited, new_x, new_y, direction, num_steps + 1, positions_visited + 1 if is_new_tile else positions_visited)
 
 def pretty_print(array, visited):
     for y in range(len(the_array)):
@@ -59,13 +65,38 @@ def pretty_print(array, visited):
 
 if __name__ == "__main__":
     the_array = read_text_file_to_array(input_file)
-    visited = [[0 for i in range(len(the_array[0]))] for j in range(len(the_array))]
+    visited_init = [[0 for i in range(len(the_array[0]))] for j in range(len(the_array))]
+
+    the_visit = copy.deepcopy(visited_init)
 
     sys.setrecursionlimit(123456789) # wtf, Python's default recursion limit is just 1000?
 
 
     guard_x, guard_y = get_guard(the_array)
-    the_direction = 0 # start heading north
-    result, num_steps, positions_visited = step(the_array, guard_x, guard_y, the_direction, 0, 1)
-    logging.info(f"result = {'EXIT' if result==1 else 'LOOP'}, num_steps = {num_steps}, positions_visited = {positions_visited}")
-    # pretty_print(the_array, visited)
+    result, num_steps, positions_visited = step(the_array, the_visit, guard_x, guard_y, 0, 0, 1)
+    logging.info(f"Task 1: result = {'EXIT' if result==1 else 'LOOP'}, num_steps = {num_steps}, positions_visited = {positions_visited}")
+
+    assert result == 1 # Unabridged maze run should be an EXIT, not a LOOP
+
+    num_loops = 0
+    num_tries = 0
+    for (x,y) in  [(x,y) for x in range(len(the_array)) for y in range(len(the_array[0])) if the_visit[y][x]>0]:
+        if (x == guard_x) and (y == guard_y):
+            # A rule forbids to place an obstacle at the guard's starting position.
+            continue
+
+        logging.debug(f"Placing test obstacle at position: x={x}, y={y}.")
+        new_visit = copy.deepcopy(visited_init)
+        assert the_array[y][x] != "#"
+        the_array[y][x] = "#"
+        result, _, _ = step(the_array, new_visit, guard_x, guard_y, 0, 0, 1)
+        the_array[y][x] = "."
+        logging.debug(f"result = {'EXIT' if result == 1 else 'LOOP'}, num_steps = {num_steps}, positions_visited = {positions_visited}")
+        if result == 2:
+            num_loops += 1
+        num_tries += 1
+        if num_tries % 100 == 0:
+            print(f"num_tries = {num_tries}")
+        
+
+    logging.info(f"Task 2: Number of possible loops by placing an additional obstacle: {num_loops}.")
